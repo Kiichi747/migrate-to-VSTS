7d412c21d4383a7d39ece13e8a72957fcfe842e7
IvanBoyko <boiko.ivan@gmail.com>
2016-09-01 12:23:07 +0100

    config.bat - minor change of order of blocks for better readability

--- Migrate_TFS_to_Git/config.bat
+++ Migrate_TFS_to_Git/config.bat
@@ -8,13 +8,14 @@ rem   > git update-index --no-assume-unchanged <file>
 
 set TFS_COLLECTION=https://@@@account@@@.visualstudio.com/@@@collection@@@
 set TFS_PATH=$/.../...
-set LOCAL_DIR=D:\...\...
 
 @rem TODO: consider getting first and last changeset ids from TFS by "tf ..." (might require to install Team Explorer Everywhere) to fetch changesets in smaller chunks
 set TFS_CHANGESET_FIRST=0
 @rem first - to fetch from the very beginning - set to 0 which will complain about invalid number but won't fetch anything at clone stage
 set TFS_CHANGESET_LAST=12
 @rem last - can be simply set to -1 to fetch all history
+
+set LOCAL_DIR=D:\...\...
 
 set GIT_AUTHOR="USERNAME <EMAILADDRESS>"
 set GIT_REPO=https://github.com/@@@user@@@/@@@repo@@@.git
