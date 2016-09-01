23e51c8852ebdbae49ed0f3f45ee9b02bb55d49d
IvanBoyko <boiko.ivan@gmail.com>
2016-09-01 09:25:30 +0100

    Removed username and password authorization as git-tfs will open TFS authorization dialog anyway

--- Migrate_TFS_to_Git/config.bat
+++ Migrate_TFS_to_Git/config.bat
@@ -1,9 +1,15 @@
 
 set TFS_COLLECTION=https://@@@account@@@.visualstudio.com/@@@collection@@@
-set TFS_USERNAME=@@@domain@@@\@@@user@@@
-set TFS_PASSWORD=@@@password@@@
 set TFS_PATH=$/.../...
 set LOCAL_DIR=D:\...\...
 
 set GIT_AUTHOR="USERNAME <EMAILADDRESS>"
 set GIT_REPO=https://github.com/@@@user@@@/@@@repo@@@.git
+@echo off
+
+rem to ignore changes in this file from git seeng them as untracked changes run:
+rem   > git update-index --assume-unchanged <file>
+rem to revert this run:
+rem   > git update-index --no-assume-unchanged <file>
+
+echo on
--- Migrate_TFS_to_Git/migrate.bat
+++ Migrate_TFS_to_Git/migrate.bat
@@ -9,11 +9,12 @@ set GIT_IGNORE_EXAMPLE_FILE=%CD%\example.gitignore
 
 
 @echo --- Time: %time%
-git tfs clone -u %TFS_USERNAME% -p %TFS_PASSWORD% --ignore-branches --resumable %TFS_COLLECTION% "%TFS_PATH%" %LOCAL_DIR%
+git tfs clone --branches=none --resumable %TFS_COLLECTION% "%TFS_PATH%" %LOCAL_DIR%
+@rem TODO: consider using: --bare, --branches=all|auto|none
 
 @echo --- Time: %time%
 cd %LOCAL_DIR%
-git tfs pull -r -u %TFS_USERNAME% -p %TFS_PASSWORD%
+git tfs pull --rebase
 
 @echo --- Time: %time%
 java -jar %BFG_JAR% --no-blob-protection --delete-files "{.git,*.dbmdl,*.1,*.2,*.bak,Thumbs.db,*.suo,*.vssscc,*.vspscc,*.vsscc,*.wixpdb,*.wixobj,*.mvfs_*,*.obj,*.pdb,*.user,*.msi}" .
@@ -24,10 +25,15 @@ java -jar %BFG_JAR% --no-blob-protection --delete-folders "{.git,Bin,bin,obj,Deb
 @echo --- Time: %time%
 java -jar %BFG_JAR% --no-blob-protection --strip-blobs-bigger-than 50M .
 
+@rem TODO: Remove the TFS source control bindings from .sln: removing the GlobalSection(TeamFoundationVersionControl) ... EndGlobalSection
+
+@rem TODO: Add a .gitattributes file
+
 @echo --- Time: %time%
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
 
+REM TODO: consider converting .tfignore to .gitignore instead of below
 @echo --- Time: %time%
 set TARGET_GITIGNORE_FILE=%LOCAL_DIR%\.gitignore
 copy %GIT_IGNORE_EXAMPLE_FILE% %TARGET_GITIGNORE_FILE%
